761ca103f8cdcaa0d86c2119c570f552d078c521
ilhamabi <33386487+ilhamabi@users.noreply.github.com>
2025-06-28 01:22:24 +0700

    Update IndexTest.php

--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -1,11 +1,11 @@
 <?php
-require_once __DIR__ . '/../src/index.php';
-
 use PHPUnit\Framework\TestCase;
+
+// Panggil fungsi dari index.php
+require_once __DIR__ . '/../src/index.php';
 
 class IndexTest extends TestCase {
     public function testGreet() {
-        $this->assertEquals("Hello, John!", greet("John"));
+        $this->assertEquals("Hello, Dunia!", greet("Dunia"));
     }
 }
-
